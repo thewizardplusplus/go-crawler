6fbbb82885d323f45e786054e712b0f51bfeb9bc
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-17 23:00:00 +0300

    Implement tests of the extractors.DefaultExtractor.ExtractLinks() method

--- extractors/default_extractor_test.go
+++ extractors/default_extractor_test.go
@@ -2,7 +2,11 @@ package extractors
 
 import (
 	"context"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"testing"
+	"testing/iotest"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -25,7 +29,137 @@ func TestDefaultExtractor_ExtractLinks(test *testing.T) {
 		args      args
 		wantLinks []string
 		wantErr   assert.ErrorAssertionFunc
-	}{} {
+	}{
+		{
+			name: "success without links",
+			fields: fields{
+				HTTPClient: func() HTTPClient {
+					request, _ := http.NewRequest(http.MethodGet, "http://example.com/", nil)
+					request = request.WithContext(context.Background())
+
+					response := &http.Response{
+						Body: ioutil.NopCloser(strings.NewReader(``)),
+					}
+
+					httpClient := new(MockHTTPClient)
+					httpClient.On("Do", request).Return(response, nil)
+
+					return httpClient
+				}(),
+				Filters: htmlselector.OptimizeFilters(htmlselector.FilterGroup{
+					"a": {"href"},
+				}),
+			},
+			args: args{
+				ctx:  context.Background(),
+				link: "http://example.com/",
+			},
+			wantLinks: nil,
+			wantErr:   assert.NoError,
+		},
+		{
+			name: "success with links",
+			fields: fields{
+				HTTPClient: func() HTTPClient {
+					request, _ := http.NewRequest(http.MethodGet, "http://example.com/", nil)
+					request = request.WithContext(context.Background())
+
+					response := &http.Response{
+						Body: ioutil.NopCloser(strings.NewReader(`
+							<ul>
+								<li><a href="http://example.com/1">1</a></li>
+								<li><a href="http://example.com/2">2</a></li>
+							</ul>
+						`)),
+					}
+
+					httpClient := new(MockHTTPClient)
+					httpClient.On("Do", request).Return(response, nil)
+
+					return httpClient
+				}(),
+				Filters: htmlselector.OptimizeFilters(htmlselector.FilterGroup{
+					"a": {"href"},
+				}),
+			},
+			args: args{
+				ctx:  context.Background(),
+				link: "http://example.com/",
+			},
+			wantLinks: []string{"http://example.com/1", "http://example.com/2"},
+			wantErr:   assert.NoError,
+		},
+		{
+			name: "error with request creating",
+			fields: fields{
+				HTTPClient: new(MockHTTPClient),
+				Filters: htmlselector.OptimizeFilters(htmlselector.FilterGroup{
+					"a": {"href"},
+				}),
+			},
+			args: args{
+				ctx:  context.Background(),
+				link: ":",
+			},
+			wantLinks: nil,
+			wantErr:   assert.Error,
+		},
+		{
+			name: "error with request sending",
+			fields: fields{
+				HTTPClient: func() HTTPClient {
+					request, _ := http.NewRequest(http.MethodGet, "http://example.com/", nil)
+					request = request.WithContext(context.Background())
+
+					httpClient := new(MockHTTPClient)
+					httpClient.On("Do", request).Return(nil, iotest.ErrTimeout)
+
+					return httpClient
+				}(),
+				Filters: htmlselector.OptimizeFilters(htmlselector.FilterGroup{
+					"a": {"href"},
+				}),
+			},
+			args: args{
+				ctx:  context.Background(),
+				link: "http://example.com/",
+			},
+			wantLinks: nil,
+			wantErr:   assert.Error,
+		},
+		{
+			name: "error with tags selecting",
+			fields: fields{
+				HTTPClient: func() HTTPClient {
+					request, _ := http.NewRequest(http.MethodGet, "http://example.com/", nil)
+					request = request.WithContext(context.Background())
+
+					response := &http.Response{
+						Body: ioutil.NopCloser(iotest.TimeoutReader(strings.NewReader(`
+							<ul>
+								<li><a href="http://example.com/1">1</a></li>
+								<li><a href="http://example.com/2">2</a></li>
+							</ul>
+						`))),
+					}
+
+					httpClient := new(MockHTTPClient)
+					httpClient.On("Do", request).Return(response, nil)
+
+					return httpClient
+				}(),
+				Filters: htmlselector.OptimizeFilters(htmlselector.FilterGroup{
+					"a": {"href"},
+				}),
+			},
+			args: args{
+				ctx:  context.Background(),
+				link: "http://example.com/",
+			},
+			wantLinks: nil,
+			wantErr:   assert.Error,
+		},
+	} {
 		test.Run(data.name, func(test *testing.T) {
 			extractor := DefaultExtractor{
 				HTTPClient: data.fields.HTTPClient,
