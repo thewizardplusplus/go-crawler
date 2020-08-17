cc38b53eb89fca855dab743cdba9dedd6dda13df
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-17 23:00:00 +0300

    Add the extractors.DefaultExtractor.ExtractLinks() method

--- extractors/default_extractor.go
+++ extractors/default_extractor.go
@@ -1,9 +1,11 @@
 package extractors
 
 import (
+	"context"
 	"net/http"
 
 	htmlselector "github.com/thewizardplusplus/go-html-selector"
+	"github.com/thewizardplusplus/go-html-selector/builders"
 )
 
 // HTTPClient ...
@@ -15,4 +17,38 @@ type HTTPClient interface {
 type DefaultExtractor struct {
 	HTTPClient HTTPClient
 	Filters    htmlselector.OptimizedFilterGroup
+}
+
+// ExtractLinks ...
+func (extractor DefaultExtractor) ExtractLinks(ctx context.Context, link string) ([]string, error) {
+	request, err := http.NewRequest(http.MethodGet, link, nil)
+	if err != nil {
+		return nil, err
+	}
+	request = request.WithContext(ctx)
+
+	response, err := extractor.HTTPClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	var builder builders.FlattenBuilder
+	if err := htmlselector.SelectTags(
+		response.Body,
+		extractor.Filters,
+		&builder,
+		htmlselector.SkipEmptyTags(),
+		htmlselector.SkipEmptyAttributes(),
+	); err != nil {
+		return nil, err
+	}
+
+	var links []string
+	for _, attributeValue := range builder.AttributeValues() {
+		link := string(attributeValue)
+		links = append(links, link)
+	}
+
+	return links, nil
 }
