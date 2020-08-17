4f0a66176fc8501722df4c56d8e2ac6069b1fb84
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-17 23:00:00 +0300

    Add the extractors.DefaultExtractor structure

--- extractors/default_extractor.go
+++ extractors/default_extractor.go
@@ -2,9 +2,17 @@ package extractors
 
 import (
 	"net/http"
+
+	htmlselector "github.com/thewizardplusplus/go-html-selector"
 )
 
 // HTTPClient ...
 type HTTPClient interface {
 	Do(request *http.Request) (*http.Response, error)
+}
+
+// DefaultExtractor ...
+type DefaultExtractor struct {
+	HTTPClient HTTPClient
+	Filters    htmlselector.OptimizedFilterGroup
 }
