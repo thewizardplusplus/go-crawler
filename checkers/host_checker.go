e5cb7cf1cc95e61f38ed5a014e0bf3f3ed25bb28
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-17 23:00:00 +0300

    Add the checkers.HostChecker.CheckLink() method

--- checkers/host_checker.go
+++ checkers/host_checker.go
@@ -1,10 +1,29 @@
 package checkers
 
 import (
+	"net/url"
+
 	"github.com/go-log/log"
 )
 
 // HostChecker ...
 type HostChecker struct {
 	Logger log.Logger
+}
+
+// CheckLink ...
+func (checker HostChecker) CheckLink(parentLink string, link string) bool {
+	parsedParentLink, err := url.Parse(parentLink)
+	if err != nil {
+		checker.Logger.Logf("unable to parse the parent link: %s", err)
+		return false
+	}
+
+	parsedLink, err := url.Parse(link)
+	if err != nil {
+		checker.Logger.Logf("unable to parse the link: %s", err)
+		return false
+	}
+
+	return parsedLink.Host == parsedParentLink.Host
 }
