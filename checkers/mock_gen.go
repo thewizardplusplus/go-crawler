c025cff3cdd21cef4edac76a46b529a3721f05ed
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-17 23:00:00 +0300

    Generate a mock for the log.Logger interface

---
+++ checkers/mock_gen.go
@@ -0,0 +1,14 @@
+package checkers
+
+import (
+	"github.com/go-log/log"
+)
+
+//go:generate mockery -name=Logger -inpkg -case=underscore -testonly
+
+// Logger ...
+//
+// It's used only for mock generating.
+type Logger interface {
+	log.Logger
+}
