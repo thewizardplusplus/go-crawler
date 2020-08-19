bac88176d9869efebf60faf482446c4ff5b8ddad
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-19 23:00:00 +0300

    Add the example

--- examples_test.go
+++ examples_test.go
@@ -1,38 +1,4 @@
-# go-crawler
-
-[![GoDoc](https://godoc.org/github.com/thewizardplusplus/go-crawler?status.svg)](https://godoc.org/github.com/thewizardplusplus/go-crawler)
-[![Go Report Card](https://goreportcard.com/badge/github.com/thewizardplusplus/go-crawler)](https://goreportcard.com/report/github.com/thewizardplusplus/go-crawler)
-[![Build Status](https://travis-ci.org/thewizardplusplus/go-crawler.svg?branch=master)](https://travis-ci.org/thewizardplusplus/go-crawler)
-[![codecov](https://codecov.io/gh/thewizardplusplus/go-crawler/branch/master/graph/badge.svg)](https://codecov.io/gh/thewizardplusplus/go-crawler)
-
-## Installation
-
-Prepare the directory:
-
-```
-$ mkdir --parents "$(go env GOPATH)/src/github.com/thewizardplusplus/"
-$ cd "$(go env GOPATH)/src/github.com/thewizardplusplus/"
-```
-
-Clone this repository:
-
-```
-$ git clone https://github.com/thewizardplusplus/go-crawler.git
-$ cd go-crawler
-```
-
-Install dependencies with the [dep](https://golang.github.io/dep/) tool:
-
-```
-$ dep ensure -vendor-only
-```
-
-## Examples
-
-`crawler.HandleLinks()`:
-
-```go
-package main
+package crawler_test
 
 import (
 	"context"
@@ -62,7 +28,7 @@ func (handler LinkHandler) HandleLink(link string) {
 	fmt.Printf("have got the link: %s\n", link)
 }
 
-func main() {
+func ExampleHandleLinks() {
 	server := httptest.NewServer(http.HandlerFunc(func(
 		writer http.ResponseWriter,
 		request *http.Request,
@@ -71,7 +37,7 @@ func main() {
 			return
 		}
 
-		fmt.Fprintf(
+		fmt.Fprintf( // nolint: errcheck
 			writer,
 			`<ul>
 				<li><a href="http://%[1]s/1">1</a></li>
@@ -116,10 +82,3 @@ func main() {
 	// have got the link: http://example.com/1
 	// have got the link: http://example.com/2
 }
-```
-
-## License
-
-The MIT License (MIT)
-
-Copyright &copy; 2020 thewizardplusplus
