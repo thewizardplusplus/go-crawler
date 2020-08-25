02f2f4630b6146a528a60faf8eab8d7a01378351
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-25 23:00:00 +0300

    Fix code style of the crawler package

--- mock_gen.go
+++ mock_gen.go
@@ -5,20 +5,22 @@ import (
 	"github.com/thewizardplusplus/go-crawler/waiter"
 )
 
-//go:generate mockery -name=Logger -inpkg -case=underscore -testonly
-
-// Logger ...
-//
-// It's used only for mock generating.
-type Logger interface {
-	log.Logger
-}
-
 //go:generate mockery -name=Waiter -inpkg -case=underscore -testonly
 
 // Waiter ...
 //
 // It's used only for mock generating.
+//
 type Waiter interface {
 	waiter.Waiter
+}
+
+//go:generate mockery -name=Logger -inpkg -case=underscore -testonly
+
+// Logger ...
+//
+// It's used only for mock generating.
+//
+type Logger interface {
+	log.Logger
 }
