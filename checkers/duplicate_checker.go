6115d4ac99a25990a027a57b53952a2b583bd8c8
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-27 23:00:00 +0300

    Add link sanitizing to the checkers.DuplicateChecker structure

--- checkers/duplicate_checker.go
+++ checkers/duplicate_checker.go
@@ -1,16 +1,34 @@
 package checkers
 
 import (
+	"net/url"
+	"path"
+
 	mapset "github.com/deckarep/golang-set"
+	"github.com/go-log/log"
 )
 
 // DuplicateChecker ...
 type DuplicateChecker struct {
+	SanitizeLink bool
+	Logger       log.Logger
+
 	checkedLinks mapset.Set
 }
 
 // CheckLink ...
 func (checker DuplicateChecker) CheckLink(parentLink string, link string) bool {
+	if checker.SanitizeLink {
+		parsedLink, err := url.Parse(link)
+		if err != nil {
+			checker.Logger.Logf("unable to parse the link: %s", err)
+			return false
+		}
+		parsedLink.Path = path.Clean(parsedLink.Path)
+
+		link = parsedLink.String()
+	}
+
 	isDuplicate := checker.checkedLinks.Contains(link)
 	checker.checkedLinks.Add(link)
 
