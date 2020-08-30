16f8da147a8954b3ea1417db962530830d51f42d
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-30 23:00:00 +0300

    Add the checkers.CheckerGroup.CheckLink() method

--- checkers/checker_group.go
+++ checkers/checker_group.go
@@ -6,3 +6,14 @@ import (
 
 // CheckerGroup ...
 type CheckerGroup []crawler.LinkChecker
+
+// CheckLink ...
+func (checkers CheckerGroup) CheckLink(parentLink string, link string) bool {
+	for _, checker := range checkers {
+		if !checker.CheckLink(parentLink, link) {
+			return false
+		}
+	}
+
+	return true
+}
