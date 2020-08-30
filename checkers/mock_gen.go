09617e1fab31ded0777a78e434d0d0d663e9eed9
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-30 23:00:00 +0300

    Generate mocks for dependencies of the checkers.CheckerGroup type

--- checkers/mock_gen.go
+++ checkers/mock_gen.go
@@ -2,13 +2,25 @@ package checkers
 
 import (
 	"github.com/go-log/log"
+	crawler "github.com/thewizardplusplus/go-crawler"
 )
+
+//go:generate mockery -name=LinkChecker -inpkg -case=underscore -testonly
+
+// LinkChecker ...
+//
+// It's used only for mock generating.
+//
+type LinkChecker interface {
+	crawler.LinkChecker
+}
 
 //go:generate mockery -name=Logger -inpkg -case=underscore -testonly
 
 // Logger ...
 //
 // It's used only for mock generating.
+//
 type Logger interface {
 	log.Logger
 }
