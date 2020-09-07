f4ea552ccd989525bb22c014d912ca83a7741f83
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-09-07 23:00:00 +0300

    Add a base of tests for the handlers.UniqueHandler.HandleLink() method

--- handlers/unique_handler_test.go
+++ handlers/unique_handler_test.go
@@ -3,9 +3,12 @@ package handlers
 import (
 	"testing"
 
+	mapset "github.com/deckarep/golang-set"
+	"github.com/go-log/log"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	crawler "github.com/thewizardplusplus/go-crawler"
 	"github.com/thewizardplusplus/go-crawler/sanitizing"
 )
 
@@ -20,4 +23,43 @@ func TestNewUniqueHandler(test *testing.T) {
 	assert.Equal(test, linkHandler, got.linkHandler)
 	assert.Equal(test, logger, got.logger)
 	assert.NotNil(test, got.handledLinks)
+}
+
+func TestUniqueHandler_HandleLink(test *testing.T) {
+	type fields struct {
+		sanitizeLink sanitizing.LinkSanitizing
+		linkHandler  crawler.LinkHandler
+		logger       log.Logger
+
+		handledLinks mapset.Set
+	}
+	type args struct {
+		sourceLink string
+		link       string
+	}
+
+	for _, data := range []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			handler := UniqueHandler{
+				sanitizeLink: data.fields.sanitizeLink,
+				linkHandler:  data.fields.linkHandler,
+				logger:       data.fields.logger,
+
+				handledLinks: data.fields.handledLinks,
+			}
+			handler.HandleLink(data.args.sourceLink, data.args.link)
+
+			mock.AssertExpectationsForObjects(
+				test,
+				data.fields.linkHandler,
+				data.fields.logger,
+			)
+		})
+	}
 }
