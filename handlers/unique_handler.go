04b68341b5b3773207f1475f95123574ab4d7097
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-09-07 23:00:00 +0300

    Add the handlers.UniqueHandler.HandleLink() method

--- handlers/unique_handler.go
+++ handlers/unique_handler.go
@@ -33,3 +33,28 @@ func NewUniqueHandler(
 		handledLinks: mapset.NewThreadUnsafeSet(),
 	}
 }
+
+// HandleLink ...
+func (handler *UniqueHandler) HandleLink(sourceLink string, link string) {
+	if handler.sanitizeLink == sanitizing.SanitizeLink {
+		var err error
+		link, err = sanitizing.ApplyLinkSanitizing(link)
+		if err != nil {
+			handler.logger.Logf("unable to sanitize the link: %s", err)
+			return
+		}
+	}
+
+	handler.locker.Lock()
+	defer handler.locker.Unlock()
+
+	// checking should be occurred under lock
+	if handler.handledLinks.Contains(link) {
+		return
+	}
+	// use the defer operator to remember a link
+	// even if the inner handler throws a panic
+	defer handler.handledLinks.Add(link)
+
+	handler.linkHandler.HandleLink(sourceLink, link)
+}
