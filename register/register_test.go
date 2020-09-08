44c15afeaed134289549decb5fd68daf45bd349f
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-09-08 23:00:00 +0300

    Add a base of tests for the register.LinkRegister.RegisterLink() method

--- register/register_test.go
+++ register/register_test.go
@@ -3,6 +3,8 @@ package register
 import (
 	"testing"
 
+	mapset "github.com/deckarep/golang-set"
+	"github.com/go-log/log"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/thewizardplusplus/go-crawler/sanitizing"
@@ -16,4 +18,38 @@ func TestNewLinkRegister(test *testing.T) {
 	assert.Equal(test, sanitizing.SanitizeLink, got.sanitizeLink)
 	assert.Equal(test, logger, got.logger)
 	assert.NotNil(test, got.registeredLinks)
+}
+
+func TestLinkRegister_RegisterLink(test *testing.T) {
+	type fields struct {
+		sanitizeLink sanitizing.LinkSanitizing
+		logger       log.Logger
+
+		registeredLinks mapset.Set
+	}
+	type args struct {
+		link string
+	}
+
+	for _, data := range []struct {
+		name   string
+		fields fields
+		args   args
+		want   assert.BoolAssertionFunc
+	}{
+		// TODO: Add test cases.
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			register := LinkRegister{
+				sanitizeLink: data.fields.sanitizeLink,
+				logger:       data.fields.logger,
+
+				registeredLinks: data.fields.registeredLinks,
+			}
+			got := register.RegisterLink(data.args.link)
+
+			mock.AssertExpectationsForObjects(test, data.fields.logger)
+			data.want(test, got)
+		})
+	}
 }
