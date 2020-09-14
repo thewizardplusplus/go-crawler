b209ecc6bffe96fca45d7afdcef2c4ed10ca136b
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-09-14 23:00:00 +0300

    Use the register.LinkRegister structure inside the handlers.UniqueHandler structure

--- handlers/unique_handler.go
+++ handlers/unique_handler.go
@@ -3,53 +3,35 @@ package handlers
 import (
 	"sync"
 
-	mapset "github.com/deckarep/golang-set"
-	"github.com/go-log/log"
 	crawler "github.com/thewizardplusplus/go-crawler"
-	"github.com/thewizardplusplus/go-crawler/sanitizing"
+	"github.com/thewizardplusplus/go-crawler/register"
 )
 
 // UniqueHandler ...
 type UniqueHandler struct {
-	sanitizeLink sanitizing.LinkSanitizing
+	locker       sync.Mutex
+	linkRegister register.LinkRegister
 	linkHandler  crawler.LinkHandler
-	logger       log.Logger
-
-	locker       sync.RWMutex
-	handledLinks mapset.Set
 }
 
 // NewUniqueHandler ...
 func NewUniqueHandler(
-	sanitizeLink sanitizing.LinkSanitizing,
+	linkRegister register.LinkRegister,
 	linkHandler crawler.LinkHandler,
-	logger log.Logger,
 ) *UniqueHandler {
 	return &UniqueHandler{
-		sanitizeLink: sanitizeLink,
+		linkRegister: linkRegister,
 		linkHandler:  linkHandler,
-		logger:       logger,
-
-		handledLinks: mapset.NewThreadUnsafeSet(),
 	}
 }
 
 // HandleLink ...
 func (handler *UniqueHandler) HandleLink(sourceLink string, link string) {
-	if handler.sanitizeLink == sanitizing.SanitizeLink {
-		var err error
-		link, err = sanitizing.ApplyLinkSanitizing(link)
-		if err != nil {
-			handler.logger.Logf("unable to sanitize the link: %s", err)
-			return
-		}
-	}
-
 	handler.locker.Lock()
 	defer handler.locker.Unlock()
 
-	wasAdded := handler.handledLinks.Add(link)
-	if !wasAdded {
+	wasRegistered := handler.linkRegister.RegisterLink(link)
+	if !wasRegistered {
 		return
 	}
 
