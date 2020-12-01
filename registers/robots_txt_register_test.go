c916b126c3e8d64d580d786a7ac7d1b375f33119
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-12-01 23:00:00 +0300

    Implement tests of the registers.makeRobotsTXTLink() function

--- registers/robots_txt_register_test.go
+++ registers/robots_txt_register_test.go
@@ -18,7 +18,75 @@ func Test_makeRobotsTXTLink(test *testing.T) {
 		wantRobotsTXTLink *url.URL
 		wantErr           assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success with a path only",
+			args: args{
+				regularLink: "http://example.com/test",
+			},
+			wantRobotsTXTLink: &url.URL{
+				Scheme: "http",
+				Host:   "example.com",
+				Path:   "/robots.txt",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "success with an HTTPS scheme",
+			args: args{
+				regularLink: "https://example.com/test",
+			},
+			wantRobotsTXTLink: &url.URL{
+				Scheme: "https",
+				Host:   "example.com",
+				Path:   "/robots.txt",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "success with an user",
+			args: args{
+				regularLink: "http://username:password@example.com/test",
+			},
+			wantRobotsTXTLink: &url.URL{
+				Scheme: "http",
+				User:   url.UserPassword("username", "password"),
+				Host:   "example.com",
+				Path:   "/robots.txt",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "success with a query",
+			args: args{
+				regularLink: "http://example.com/test?key=value",
+			},
+			wantRobotsTXTLink: &url.URL{
+				Scheme: "http",
+				Host:   "example.com",
+				Path:   "/robots.txt",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "success with a fragment",
+			args: args{
+				regularLink: "http://example.com/test#fragment",
+			},
+			wantRobotsTXTLink: &url.URL{
+				Scheme: "http",
+				Host:   "example.com",
+				Path:   "/robots.txt",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "error",
+			args: args{
+				regularLink: ":",
+			},
+			wantRobotsTXTLink: nil,
+			wantErr:           assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			gotRobotsTXTLink, gotErr := makeRobotsTXTLink(data.args.regularLink)
