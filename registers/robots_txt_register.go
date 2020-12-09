bd9b0345bb93984888cb374a200f3cf38a49fed4
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-12-09 23:00:00 +0300

    Add the registers.loadRobotsTXTData() function

--- registers/robots_txt_register.go
+++ registers/robots_txt_register.go
@@ -1,10 +1,12 @@
 package registers
 
 import (
+	"context"
 	"net/http"
 	"net/url"
 
 	"github.com/pkg/errors"
+	"github.com/temoto/robotstxt"
 )
 
 //go:generate mockery -name=HTTPClient -inpkg -case=underscore -testonly
@@ -27,4 +29,32 @@ func makeRobotsTXTLink(regularLink string) (robotsTXTLink string, err error) {
 		Path:   "/robots.txt",
 	}
 	return parsedRobotsTXTLink.String(), nil
+}
+
+func loadRobotsTXTData(
+	ctx context.Context,
+	httpClient HTTPClient,
+	robotsTXTLink string,
+) (
+	*robotstxt.RobotsData,
+	error,
+) {
+	request, err := http.NewRequest(http.MethodGet, robotsTXTLink, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to create the request")
+	}
+	request = request.WithContext(ctx)
+
+	response, err := httpClient.Do(request)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to send the request")
+	}
+	defer response.Body.Close() // nolint: errcheck
+
+	robotsTXTData, err := robotstxt.FromResponse(response)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to parse the response")
+	}
+
+	return robotsTXTData, nil
 }
