01962be63cef008bde34968ef87c9875f8af0772
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-01-20 23:00:00 +0300

    Add the checkers.RobotsTXTChecker.CheckLink() method

--- checkers/robots_txt_checker.go
+++ checkers/robots_txt_checker.go
@@ -1,7 +1,11 @@
 package checkers
 
 import (
+	"context"
+	"net/url"
+
 	"github.com/go-log/log"
+	crawler "github.com/thewizardplusplus/go-crawler"
 	"github.com/thewizardplusplus/go-crawler/registers"
 )
 
@@ -10,4 +14,23 @@ type RobotsTXTChecker struct {
 	UserAgent         string
 	RobotsTXTRegister registers.RobotsTXTRegister
 	Logger            log.Logger
+}
+
+// CheckLink ...
+func (checker RobotsTXTChecker) CheckLink(link crawler.SourcedLink) bool {
+	parsedLink, err := url.Parse(link.Link)
+	if err != nil {
+		checker.Logger.Logf("unable to parse the link: %s", err)
+		return false
+	}
+
+	robotsTXTData, err :=
+		checker.RobotsTXTRegister.RegisterRobotsTXT(context.Background(), link.Link)
+	if err != nil {
+		checker.Logger.Logf("unable to register the robots.txt link: %s", err)
+		return false
+	}
+
+	group := robotsTXTData.FindGroup(checker.UserAgent)
+	return group.Test(parsedLink.Path)
 }
