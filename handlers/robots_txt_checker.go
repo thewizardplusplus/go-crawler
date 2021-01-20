2c2b67f89f91c98600ac1bbe2311ddd20ed025cd
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-01-20 23:00:00 +0300

    Add the handlers.RobotsTXTHandler.HandleLink() method

--- handlers/robots_txt_checker.go
+++ handlers/robots_txt_checker.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"context"
+	"net/url"
+
 	"github.com/go-log/log"
 	crawler "github.com/thewizardplusplus/go-crawler"
 	"github.com/thewizardplusplus/go-crawler/registers"
@@ -12,4 +15,27 @@ type RobotsTXTHandler struct {
 	RobotsTXTRegister registers.RobotsTXTRegister
 	LinkHandler       crawler.LinkHandler
 	Logger            log.Logger
+}
+
+// HandleLink ...
+func (handler RobotsTXTHandler) HandleLink(link crawler.SourcedLink) {
+	parsedLink, err := url.Parse(link.Link)
+	if err != nil {
+		handler.Logger.Logf("unable to parse the link: %s", err)
+		return
+	}
+
+	robotsTXTData, err :=
+		handler.RobotsTXTRegister.RegisterRobotsTXT(context.Background(), link.Link)
+	if err != nil {
+		handler.Logger.Logf("unable to register the robots.txt link: %s", err)
+		return
+	}
+
+	group := robotsTXTData.FindGroup(handler.UserAgent)
+	if !group.Test(parsedLink.Path) {
+		return
+	}
+
+	handler.LinkHandler.HandleLink(link)
 }
