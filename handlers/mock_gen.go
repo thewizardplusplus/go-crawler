8e1bb16a3e58e155f9fe77b4b676caae994fdd61
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-02-04 23:00:00 +0300

    Generate a mock for the crawler.LinkChecker interface in the handlers package

--- handlers/mock_gen.go
+++ handlers/mock_gen.go
@@ -7,6 +7,16 @@ import (
 	crawler "github.com/thewizardplusplus/go-crawler"
 )
 
+//go:generate mockery -name=LinkChecker -inpkg -case=underscore -testonly
+
+// LinkChecker ...
+//
+// It's used only for mock generating.
+//
+type LinkChecker interface {
+	crawler.LinkChecker
+}
+
 //go:generate mockery -name=LinkHandler -inpkg -case=underscore -testonly
 
 // LinkHandler ...
