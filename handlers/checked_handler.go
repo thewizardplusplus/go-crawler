5f80a1bb4bdebb12b3a8020669a96eba7cd78d90
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-02-04 23:00:00 +0300

    Add the handlers.CheckedHandler.HandleLink() method

--- handlers/checked_handler.go
+++ handlers/checked_handler.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"context"
+
 	crawler "github.com/thewizardplusplus/go-crawler"
 )
 
@@ -8,4 +10,16 @@ import (
 type CheckedHandler struct {
 	LinkChecker crawler.LinkChecker
 	LinkHandler crawler.LinkHandler
+}
+
+// HandleLink ...
+func (handler CheckedHandler) HandleLink(
+	ctx context.Context,
+	link crawler.SourcedLink,
+) {
+	if !handler.LinkChecker.CheckLink(ctx, link) {
+		return
+	}
+
+	handler.LinkHandler.HandleLink(ctx, link)
 }
