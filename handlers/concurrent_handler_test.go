589e73df6406be20c5b8229e89359b881c30c969
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-02-15 23:00:00 +0300

    Add tests for the handlers.ConcurrentHandler.HandleLink() method

--- handlers/concurrent_handler_test.go
+++ handlers/concurrent_handler_test.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	crawler "github.com/thewizardplusplus/go-crawler"
 )
 
 func TestNewConcurrentHandler(test *testing.T) {
@@ -16,4 +18,18 @@ func TestNewConcurrentHandler(test *testing.T) {
 	assert.NotNil(test, handler.links)
 	assert.Len(test, handler.links, 0)
 	assert.Equal(test, 1000, cap(handler.links))
+}
+
+func TestConcurrentHandler_HandleLink(test *testing.T) {
+	link := crawler.SourcedLink{
+		SourceLink: "http://example.com/",
+		Link:       "http://example.com/test",
+	}
+
+	links := make(chan crawler.SourcedLink, 1)
+	handler := ConcurrentHandler{links: links}
+	handler.HandleLink(context.Background(), link)
+
+	gotLink := <-handler.links
+	assert.Equal(test, link, gotLink)
 }
