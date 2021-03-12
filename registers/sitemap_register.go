6bdc709754ddd3cced997a228b4e0fef136d70eb
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-12 23:00:00 +0300

    Add the registers.SitemapRegister.loadSitemapData() method

--- registers/sitemap_register.go
+++ registers/sitemap_register.go
@@ -1,7 +1,11 @@
 package registers
 
 import (
+	"context"
 	"sync"
+
+	"github.com/pkg/errors"
+	"github.com/yterajima/go-sitemap"
 )
 
 // LinkGenerator ...
@@ -14,4 +18,25 @@ type SitemapRegister struct {
 	linkGenerator LinkGenerator
 
 	registeredSitemaps *sync.Map
+}
+
+func (register SitemapRegister) loadSitemapData(
+	ctx context.Context,
+	sitemapLink string,
+) (
+	sitemap.Sitemap,
+	error,
+) {
+	sitemapData, ok := register.registeredSitemaps.Load(sitemapLink)
+	if !ok {
+		var err error
+		sitemapData, err = sitemap.Get(sitemapLink, ctx)
+		if err != nil {
+			return sitemap.Sitemap{}, errors.Wrap(err, "unable to load the Sitemap data")
+		}
+
+		register.registeredSitemaps.Store(sitemapLink, sitemapData)
+	}
+
+	return sitemapData.(sitemap.Sitemap), nil
 }
