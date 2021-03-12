dcfabf5af2353b3b14605e2d18aad23523ebf99b
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-12 23:00:00 +0300

    Add the registers.SitemapRegister structure

--- registers/sitemap_register.go
+++ registers/sitemap_register.go
@@ -1,6 +1,17 @@
 package registers
 
+import (
+	"sync"
+)
+
 // LinkGenerator ...
 type LinkGenerator interface {
 	GenerateLinks(baseLink string) ([]string, error)
+}
+
+// SitemapRegister ...
+type SitemapRegister struct {
+	linkGenerator LinkGenerator
+
+	registeredSitemaps *sync.Map
 }
