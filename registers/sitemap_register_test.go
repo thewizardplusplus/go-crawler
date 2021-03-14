77ff937beed634a6d6ffbddfe902449f90106632
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-14 23:00:00 +0300

    Implement tests of the registers.SitemapRegister.RegisterSitemap() method

--- registers/sitemap_register_test.go
+++ registers/sitemap_register_test.go
@@ -3,11 +3,13 @@ package registers
 import (
 	"context"
 	"encoding/xml"
+	"reflect"
 	"sync"
 	"testing"
 	"testing/iotest"
 
 	"github.com/go-log/log"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/yterajima/go-sitemap"
@@ -32,7 +34,164 @@ func TestSitemapRegister_RegisterSitemap(test *testing.T) {
 		wantSitemapData sitemap.Sitemap
 		wantErr         assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success with all successful links",
+			fields: fields{
+				linkGenerator: func() LinkGenerator {
+					sitemapLinks := []string{
+						"http://example.com/sitemap_1.xml",
+						"http://example.com/sitemap_2.xml",
+					}
+
+					linkGenerator := new(MockLinkGenerator)
+					linkGenerator.
+						On("GenerateLinks", "http://example.com/test").
+						Return(sitemapLinks, nil)
+
+					return linkGenerator
+				}(),
+				linkLoader: func() LinkLoader {
+					const responseOne = `
+						<?xml version="1.0" encoding="UTF-8" ?>
+						<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+							<url>
+								<loc>http://example.com/1</loc>
+							</url>
+							<url>
+								<loc>http://example.com/2</loc>
+							</url>
+						</urlset>
+					`
+					const responseTwo = `
+						<?xml version="1.0" encoding="UTF-8" ?>
+						<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+							<url>
+								<loc>http://example.com/3</loc>
+							</url>
+							<url>
+								<loc>http://example.com/4</loc>
+							</url>
+						</urlset>
+					`
+
+					linkLoader := new(MockLinkLoader)
+					linkLoader.
+						On("LoadLink", "http://example.com/sitemap_1.xml", context.Background()).
+						Return([]byte(responseOne), nil)
+					linkLoader.
+						On("LoadLink", "http://example.com/sitemap_2.xml", context.Background()).
+						Return([]byte(responseTwo), nil)
+
+					return linkLoader
+				}(),
+				logger:             new(MockLogger),
+				registeredSitemaps: new(sync.Map),
+			},
+			args: args{
+				ctx:  context.Background(),
+				link: "http://example.com/test",
+			},
+			wantSitemapData: sitemap.Sitemap{
+				URL: []sitemap.URL{
+					{Loc: "http://example.com/1"},
+					{Loc: "http://example.com/2"},
+					{Loc: "http://example.com/3"},
+					{Loc: "http://example.com/4"},
+				},
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "success with some failed links",
+			fields: fields{
+				linkGenerator: func() LinkGenerator {
+					sitemapLinks := []string{
+						"http://example.com/sitemap_1.xml",
+						"http://example.com/sitemap_2.xml",
+					}
+
+					linkGenerator := new(MockLinkGenerator)
+					linkGenerator.
+						On("GenerateLinks", "http://example.com/test").
+						Return(sitemapLinks, nil)
+
+					return linkGenerator
+				}(),
+				linkLoader: func() LinkLoader {
+					const responseOne = `
+						<?xml version="1.0" encoding="UTF-8" ?>
+						<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+							<url>
+								<loc>http://example.com/1</loc>
+							</url>
+							<url>
+								<loc>http://example.com/2</loc>
+							</url>
+						</urlset>
+					`
+
+					linkLoader := new(MockLinkLoader)
+					linkLoader.
+						On("LoadLink", "http://example.com/sitemap_1.xml", context.Background()).
+						Return([]byte(responseOne), nil)
+					linkLoader.
+						On("LoadLink", "http://example.com/sitemap_2.xml", context.Background()).
+						Return(nil, iotest.ErrTimeout)
+
+					return linkLoader
+				}(),
+				logger: func() Logger {
+					logger := new(MockLogger)
+					logger.
+						On(
+							"Logf",
+							"unable to process the Sitemap link %q: %s",
+							"http://example.com/sitemap_2.xml",
+							mock.MatchedBy(func(err error) bool {
+								unwrappedErr := errors.Cause(err)
+								return reflect.DeepEqual(unwrappedErr, iotest.ErrTimeout)
+							}),
+						).
+						Return()
+
+					return logger
+				}(),
+				registeredSitemaps: new(sync.Map),
+			},
+			args: args{
+				ctx:  context.Background(),
+				link: "http://example.com/test",
+			},
+			wantSitemapData: sitemap.Sitemap{
+				URL: []sitemap.URL{
+					{Loc: "http://example.com/1"},
+					{Loc: "http://example.com/2"},
+				},
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "error",
+			fields: fields{
+				linkGenerator: func() LinkGenerator {
+					linkGenerator := new(MockLinkGenerator)
+					linkGenerator.
+						On("GenerateLinks", "http://example.com/test").
+						Return(nil, iotest.ErrTimeout)
+
+					return linkGenerator
+				}(),
+				linkLoader:         new(MockLinkLoader),
+				logger:             new(MockLogger),
+				registeredSitemaps: new(sync.Map),
+			},
+			args: args{
+				ctx:  context.Background(),
+				link: "http://example.com/test",
+			},
+			wantSitemapData: sitemap.Sitemap{},
+			wantErr:         assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			sitemap.SetFetch(data.fields.linkLoader.LoadLink)
