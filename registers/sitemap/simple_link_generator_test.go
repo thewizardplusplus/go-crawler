1088a2b729e43f4a9f03d9834b1c18cc64f425dc
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-04-26 23:00:00 +0300

    Implement tests of the sitemap.SimpleLinkGenerator.GenerateLinks() method

--- registers/sitemap/simple_link_generator_test.go
+++ registers/sitemap/simple_link_generator_test.go
@@ -17,7 +17,56 @@ func TestSimpleLinkGenerator_GenerateLinks(test *testing.T) {
 		wantSitemapLinks []string
 		wantErr          assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success with a path only",
+			args: args{
+				baseLink: "http://example.com/test",
+			},
+			wantSitemapLinks: []string{"http://example.com/sitemap.xml"},
+			wantErr:          assert.NoError,
+		},
+		{
+			name: "success with an HTTPS scheme",
+			args: args{
+				baseLink: "https://example.com/test",
+			},
+			wantSitemapLinks: []string{"https://example.com/sitemap.xml"},
+			wantErr:          assert.NoError,
+		},
+		{
+			name: "success with an user",
+			args: args{
+				baseLink: "http://username:password@example.com/test",
+			},
+			wantSitemapLinks: []string{
+				"http://username:password@example.com/sitemap.xml",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "success with a query",
+			args: args{
+				baseLink: "http://example.com/test?key=value",
+			},
+			wantSitemapLinks: []string{"http://example.com/sitemap.xml"},
+			wantErr:          assert.NoError,
+		},
+		{
+			name: "success with a fragment",
+			args: args{
+				baseLink: "http://example.com/test#fragment",
+			},
+			wantSitemapLinks: []string{"http://example.com/sitemap.xml"},
+			wantErr:          assert.NoError,
+		},
+		{
+			name: "error",
+			args: args{
+				baseLink: ":",
+			},
+			wantSitemapLinks: nil,
+			wantErr:          assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			var generator SimpleLinkGenerator
