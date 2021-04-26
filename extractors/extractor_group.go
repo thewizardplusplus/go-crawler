44c6afc9958ddb0d7abb71f2c9e5c94464a1403b
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-04-26 23:00:00 +0300

    Add the extractors.ExtractorGroup.ExtractLinks() method

--- extractors/extractor_group.go
+++ extractors/extractor_group.go
@@ -1,8 +1,30 @@
 package extractors
 
 import (
+	"context"
+
+	"github.com/pkg/errors"
 	"github.com/thewizardplusplus/go-crawler/models"
 )
 
 // ExtractorGroup ...
 type ExtractorGroup []models.LinkExtractor
+
+// ExtractLinks ...
+func (extractors ExtractorGroup) ExtractLinks(
+	ctx context.Context,
+	threadID int,
+	link string,
+) ([]string, error) {
+	var totalLinks []string
+	for _, extractor := range extractors {
+		links, err := extractor.ExtractLinks(ctx, threadID, link)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to extract links")
+		}
+
+		totalLinks = append(totalLinks, links...)
+	}
+
+	return totalLinks, nil
+}
