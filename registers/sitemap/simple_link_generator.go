db33e9dc366d1696e68c40640b4b2e196f00a3f8
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-04-26 23:00:00 +0300

    Add the sitemap.SimpleLinkGenerator.GenerateLinks() method

--- registers/sitemap/simple_link_generator.go
+++ registers/sitemap/simple_link_generator.go
@@ -1,4 +1,29 @@
 package sitemap
 
+import (
+	"net/url"
+
+	"github.com/pkg/errors"
+)
+
 // SimpleLinkGenerator ...
 type SimpleLinkGenerator struct{}
+
+// GenerateLinks ...
+func (generator SimpleLinkGenerator) GenerateLinks(baseLink string) (
+	[]string,
+	error,
+) {
+	parsedBaseLink, err := url.Parse(baseLink)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to parse the base link")
+	}
+
+	parsedSitemapLink := &url.URL{
+		Scheme: parsedBaseLink.Scheme,
+		User:   parsedBaseLink.User,
+		Host:   parsedBaseLink.Host,
+		Path:   "/sitemap.xml",
+	}
+	return []string{parsedSitemapLink.String()}, nil
+}
