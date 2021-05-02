1cd69c5b505cad8da022cf0870fa697c3982cfce
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-02 23:00:00 +0300

    Add the extractors.SitemapExtractor.ExtractLinks() method

--- extractors/sitemap_extractor.go
+++ extractors/sitemap_extractor.go
@@ -1,6 +1,8 @@
 package extractors
 
 import (
+	"context"
+
 	"github.com/go-log/log"
 	"github.com/thewizardplusplus/go-crawler/registers"
 )
@@ -9,4 +11,24 @@ import (
 type SitemapExtractor struct {
 	SitemapRegister registers.SitemapRegister
 	Logger          log.Logger
+}
+
+// ExtractLinks ...
+func (extractor SitemapExtractor) ExtractLinks(
+	ctx context.Context,
+	threadID int,
+	link string,
+) ([]string, error) {
+	sitemapData, err := extractor.SitemapRegister.RegisterSitemap(ctx, link)
+	if err != nil {
+		extractor.Logger.Logf("unable to register the sitemap.xml link: %s", err)
+		return nil, nil
+	}
+
+	var links []string
+	for _, url := range sitemapData.URL {
+		links = append(links, url.Loc)
+	}
+
+	return links, nil
 }
