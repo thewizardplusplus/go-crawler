6e598aa58305d050fc30afec09245279c1bdd558
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-06-10 23:00:00 +0300

    Implement tests of the sitemap.RobotsTXTGenerator.GenerateLinks() method

--- registers/sitemap/robots_txt_generator_test.go
+++ registers/sitemap/robots_txt_generator_test.go
@@ -2,7 +2,11 @@ package sitemap
 
 import (
 	"context"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"testing"
+	"testing/iotest"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/thewizardplusplus/go-crawler/registers"
@@ -24,7 +28,108 @@ func TestRobotsTXTGenerator_GenerateLinks(test *testing.T) {
 		wantSitemapLinks []string
 		wantErr          assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success without Sitemap links",
+			fields: fields{
+				RobotsTXTRegister: func() registers.RobotsTXTRegister {
+					request, _ :=
+						http.NewRequest(http.MethodGet, "http://example.com/robots.txt", nil)
+					request = request.WithContext(context.Background())
+
+					response := &http.Response{
+						StatusCode: http.StatusOK,
+						Body: ioutil.NopCloser(strings.NewReader(`
+							User-agent: *
+							Disallow: /
+
+							User-agent: go-crawler
+							Disallow: /
+							Allow: /$
+							Allow: /sitemap.xml$
+							Allow: /post/
+							Allow: /storage/app/media/
+						`)),
+					}
+
+					httpClient := new(MockHTTPClient)
+					httpClient.On("Do", request).Return(response, nil)
+
+					register := registers.NewRobotsTXTRegister(httpClient)
+					return register
+				}(),
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: "http://example.com/test",
+			},
+			wantSitemapLinks: nil,
+			wantErr:          assert.NoError,
+		},
+		{
+			name: "success with Sitemap links",
+			fields: fields{
+				RobotsTXTRegister: func() registers.RobotsTXTRegister {
+					request, _ :=
+						http.NewRequest(http.MethodGet, "http://example.com/robots.txt", nil)
+					request = request.WithContext(context.Background())
+
+					response := &http.Response{
+						StatusCode: http.StatusOK,
+						Body: ioutil.NopCloser(strings.NewReader(`
+							User-agent: *
+							Disallow: /
+
+							User-agent: go-crawler
+							Disallow: /
+							Allow: /$
+							Allow: /sitemap.xml$
+							Allow: /post/
+							Allow: /storage/app/media/
+
+							Sitemap: http://example.com/sitemap_1.xml
+							Sitemap: http://example.com/sitemap_2.xml
+						`)),
+					}
+
+					httpClient := new(MockHTTPClient)
+					httpClient.On("Do", request).Return(response, nil)
+
+					register := registers.NewRobotsTXTRegister(httpClient)
+					return register
+				}(),
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: "http://example.com/test",
+			},
+			wantSitemapLinks: []string{
+				"http://example.com/sitemap_1.xml",
+				"http://example.com/sitemap_2.xml",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "error",
+			fields: fields{
+				RobotsTXTRegister: func() registers.RobotsTXTRegister {
+					request, _ :=
+						http.NewRequest(http.MethodGet, "http://example.com/robots.txt", nil)
+					request = request.WithContext(context.Background())
+
+					httpClient := new(MockHTTPClient)
+					httpClient.On("Do", request).Return(nil, iotest.ErrTimeout)
+
+					register := registers.NewRobotsTXTRegister(httpClient)
+					return register
+				}(),
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: "http://example.com/test",
+			},
+			wantSitemapLinks: nil,
+			wantErr:          assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			generator := RobotsTXTGenerator{
