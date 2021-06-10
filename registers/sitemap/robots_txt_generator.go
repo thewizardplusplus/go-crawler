c1e3a302624f3d7a4fa84b2851a5f25211f3eb0f
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-06-10 23:00:00 +0300

    Add the sitemap.RobotsTXTGenerator structure

---
+++ registers/sitemap/robots_txt_generator.go
@@ -0,0 +1,10 @@
+package sitemap
+
+import (
+	"github.com/thewizardplusplus/go-crawler/registers"
+)
+
+// RobotsTXTGenerator ...
+type RobotsTXTGenerator struct {
+	RobotsTXTRegister registers.RobotsTXTRegister
+}
