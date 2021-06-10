2b1a4692eb36e371c353e7880d7441103ca668cb
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-06-10 23:00:00 +0300

    Add the sitemap.RobotsTXTGenerator.GenerateLinks() method

--- registers/sitemap/robots_txt_generator.go
+++ registers/sitemap/robots_txt_generator.go
@@ -1,10 +1,30 @@
 package sitemap
 
 import (
+	"context"
+
+	"github.com/pkg/errors"
 	"github.com/thewizardplusplus/go-crawler/registers"
 )
 
 // RobotsTXTGenerator ...
 type RobotsTXTGenerator struct {
 	RobotsTXTRegister registers.RobotsTXTRegister
+}
+
+// GenerateLinks ...
+func (generator RobotsTXTGenerator) GenerateLinks(
+	ctx context.Context,
+	baseLink string,
+) (
+	[]string,
+	error,
+) {
+	robotsTXTData, err :=
+		generator.RobotsTXTRegister.RegisterRobotsTXT(ctx, baseLink)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to register the robots.txt link")
+	}
+
+	return robotsTXTData.Sitemaps, nil
 }
