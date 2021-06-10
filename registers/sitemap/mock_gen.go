07b2fb6e682609f7c56a145b522a3333064f9952
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-06-10 23:00:00 +0300

    Generate a mock for the httputils.HTTPClient interface for the sitemap package

--- registers/sitemap/mock_gen.go
+++ registers/sitemap/mock_gen.go
@@ -2,6 +2,7 @@ package sitemap
 
 import (
 	"github.com/thewizardplusplus/go-crawler/registers"
+	httputils "github.com/thewizardplusplus/go-http-utils"
 )
 
 //go:generate mockery --name=LinkGenerator --inpackage --case=underscore --testonly
@@ -12,4 +13,14 @@ import (
 //
 type LinkGenerator interface {
 	registers.LinkGenerator
+}
+
+//go:generate mockery --name=HTTPClient --inpackage --case=underscore --testonly
+
+// HTTPClient ...
+//
+// It's used only for mock generating.
+//
+type HTTPClient interface {
+	httputils.HTTPClient
 }
