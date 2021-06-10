6115072bc0e2b6cb6d0e8e5b400106af5ec6db7e
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-06-10 23:00:00 +0300

    Implement tests of the sitemap.GeneratorGroup.GenerateLinks() method

--- registers/sitemap/generator_group_test.go
+++ registers/sitemap/generator_group_test.go
@@ -3,8 +3,11 @@ package sitemap
 import (
 	"context"
 	"testing"
+	"testing/iotest"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/thewizardplusplus/go-crawler/registers"
 )
 
 func TestGeneratorGroup_GenerateLinks(test *testing.T) {
@@ -20,7 +23,132 @@ func TestGeneratorGroup_GenerateLinks(test *testing.T) {
 		wantSitemapLinks []string
 		wantErr          assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name:       "empty",
+			generators: nil,
+			args: args{
+				ctx:      context.Background(),
+				baseLink: "http://example.com/test",
+			},
+			wantSitemapLinks: nil,
+			wantErr:          assert.NoError,
+		},
+		{
+			name: "without failed generatings",
+			generators: GeneratorGroup{
+				func() registers.LinkGenerator {
+					ctxMatcher := mock.MatchedBy(func(context.Context) bool { return true })
+
+					generator := new(MockLinkGenerator)
+					generator.
+						On("GenerateLinks", ctxMatcher, "http://example.com/test").
+						Return(
+							[]string{
+								"http://example.com/sitemap_1.xml",
+								"http://example.com/sitemap_2.xml",
+							},
+							nil,
+						)
+
+					return generator
+				}(),
+				func() registers.LinkGenerator {
+					ctxMatcher := mock.MatchedBy(func(context.Context) bool { return true })
+
+					generator := new(MockLinkGenerator)
+					generator.
+						On("GenerateLinks", ctxMatcher, "http://example.com/test").
+						Return(
+							[]string{
+								"http://example.com/sitemap_3.xml",
+								"http://example.com/sitemap_4.xml",
+							},
+							nil,
+						)
+
+					return generator
+				}(),
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: "http://example.com/test",
+			},
+			wantSitemapLinks: []string{
+				"http://example.com/sitemap_1.xml",
+				"http://example.com/sitemap_2.xml",
+				"http://example.com/sitemap_3.xml",
+				"http://example.com/sitemap_4.xml",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "with some failed generatings",
+			generators: GeneratorGroup{
+				func() registers.LinkGenerator {
+					ctxMatcher := mock.MatchedBy(func(context.Context) bool { return true })
+
+					generator := new(MockLinkGenerator)
+					generator.
+						On("GenerateLinks", ctxMatcher, "http://example.com/test").
+						Return(nil, iotest.ErrTimeout)
+
+					return generator
+				}(),
+				func() registers.LinkGenerator {
+					ctxMatcher := mock.MatchedBy(func(context.Context) bool { return true })
+
+					generator := new(MockLinkGenerator)
+					generator.
+						On("GenerateLinks", ctxMatcher, "http://example.com/test").
+						Return(
+							[]string{
+								"http://example.com/sitemap_3.xml",
+								"http://example.com/sitemap_4.xml",
+							},
+							nil,
+						)
+
+					return generator
+				}(),
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: "http://example.com/test",
+			},
+			wantSitemapLinks: nil,
+			wantErr:          assert.Error,
+		},
+		{
+			name: "with all failed generatings",
+			generators: GeneratorGroup{
+				func() registers.LinkGenerator {
+					ctxMatcher := mock.MatchedBy(func(context.Context) bool { return true })
+
+					generator := new(MockLinkGenerator)
+					generator.
+						On("GenerateLinks", ctxMatcher, "http://example.com/test").
+						Return(nil, iotest.ErrTimeout)
+
+					return generator
+				}(),
+				func() registers.LinkGenerator {
+					ctxMatcher := mock.MatchedBy(func(context.Context) bool { return true })
+
+					generator := new(MockLinkGenerator)
+					generator.
+						On("GenerateLinks", ctxMatcher, "http://example.com/test").
+						Return(nil, iotest.ErrTimeout)
+
+					return generator
+				}(),
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: "http://example.com/test",
+			},
+			wantSitemapLinks: nil,
+			wantErr:          assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			gotSitemapLinks, gotErr :=
