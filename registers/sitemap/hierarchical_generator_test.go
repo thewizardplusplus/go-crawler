23111ffc429f5e6bdec2cc72fb3a62d0c5523b23
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-06-13 23:00:00 +0300

    Implement tests of the sitemap.HierarchicalGenerator.GenerateLinks() method

--- registers/sitemap/hierarchical_generator_test.go
+++ registers/sitemap/hierarchical_generator_test.go
@@ -24,7 +24,138 @@ func TestHierarchicalGenerator_GenerateLinks(test *testing.T) {
 		wantSitemapLinks []string
 		wantErr          assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success without a trailing slash",
+			fields: fields{
+				SanitizeLink: sanitizing.DoNotSanitizeLink,
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: "http://example.com/test",
+			},
+			wantSitemapLinks: []string{"http://example.com/sitemap.xml"},
+			wantErr:          assert.NoError,
+		},
+		{
+			name: "success with a trailing slash",
+			fields: fields{
+				SanitizeLink: sanitizing.DoNotSanitizeLink,
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: "http://example.com/test/",
+			},
+			wantSitemapLinks: []string{
+				"http://example.com/sitemap.xml",
+				"http://example.com/test/sitemap.xml",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "success with a long path",
+			fields: fields{
+				SanitizeLink: sanitizing.DoNotSanitizeLink,
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: "http://example.com/one/two/test",
+			},
+			wantSitemapLinks: []string{
+				"http://example.com/sitemap.xml",
+				"http://example.com/one/sitemap.xml",
+				"http://example.com/one/two/sitemap.xml",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "success with an HTTPS scheme",
+			fields: fields{
+				SanitizeLink: sanitizing.DoNotSanitizeLink,
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: "https://example.com/test",
+			},
+			wantSitemapLinks: []string{"https://example.com/sitemap.xml"},
+			wantErr:          assert.NoError,
+		},
+		{
+			name: "success with an user",
+			fields: fields{
+				SanitizeLink: sanitizing.DoNotSanitizeLink,
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: "http://username:password@example.com/test",
+			},
+			wantSitemapLinks: []string{
+				"http://username:password@example.com/sitemap.xml",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "success with a query",
+			fields: fields{
+				SanitizeLink: sanitizing.DoNotSanitizeLink,
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: "http://example.com/test?key=value",
+			},
+			wantSitemapLinks: []string{"http://example.com/sitemap.xml"},
+			wantErr:          assert.NoError,
+		},
+		{
+			name: "success with a fragment",
+			fields: fields{
+				SanitizeLink: sanitizing.DoNotSanitizeLink,
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: "http://example.com/test#fragment",
+			},
+			wantSitemapLinks: []string{"http://example.com/sitemap.xml"},
+			wantErr:          assert.NoError,
+		},
+		{
+			name: "success with sanitizing",
+			fields: fields{
+				SanitizeLink: sanitizing.SanitizeLink,
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: "http://example.com/one/two/../test",
+			},
+			wantSitemapLinks: []string{
+				"http://example.com/sitemap.xml",
+				"http://example.com/one/sitemap.xml",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "error with link sanitizing",
+			fields: fields{
+				SanitizeLink: sanitizing.SanitizeLink,
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: ":",
+			},
+			wantSitemapLinks: nil,
+			wantErr:          assert.Error,
+		},
+		{
+			name: "error with link parsing",
+			fields: fields{
+				SanitizeLink: sanitizing.DoNotSanitizeLink,
+			},
+			args: args{
+				ctx:      context.Background(),
+				baseLink: ":",
+			},
+			wantSitemapLinks: nil,
+			wantErr:          assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			generator := HierarchicalGenerator{
