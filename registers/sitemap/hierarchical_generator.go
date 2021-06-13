9a64b0cbe45f800db67c07be4e19026934dc4851
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-06-13 23:00:00 +0300

    Add the sitemap.HierarchicalGenerator structure

---
+++ registers/sitemap/hierarchical_generator.go
@@ -0,0 +1,10 @@
+package sitemap
+
+import (
+	"github.com/thewizardplusplus/go-crawler/sanitizing"
+)
+
+// HierarchicalGenerator ...
+type HierarchicalGenerator struct {
+	SanitizeLink sanitizing.LinkSanitizing
+}
