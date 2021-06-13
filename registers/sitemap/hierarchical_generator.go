c249045cda65bbd28f6ff75154d86b7ce7a1f797
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-06-13 23:00:00 +0300

    Add the sitemap.HierarchicalGenerator.GenerateLinks() method

--- registers/sitemap/hierarchical_generator.go
+++ registers/sitemap/hierarchical_generator.go
@@ -1,10 +1,56 @@
 package sitemap
 
 import (
+	"context"
+	"net/url"
+	"strings"
+
+	"github.com/pkg/errors"
 	"github.com/thewizardplusplus/go-crawler/sanitizing"
 )
 
 // HierarchicalGenerator ...
 type HierarchicalGenerator struct {
 	SanitizeLink sanitizing.LinkSanitizing
+}
+
+// GenerateLinks ...
+func (generator HierarchicalGenerator) GenerateLinks(
+	ctx context.Context,
+	baseLink string,
+) (
+	[]string,
+	error,
+) {
+	if generator.SanitizeLink == sanitizing.SanitizeLink {
+		var err error
+		baseLink, err = sanitizing.ApplyLinkSanitizing(baseLink)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to sanitize the base link")
+		}
+	}
+
+	parsedBaseLink, err := url.Parse(baseLink)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to parse the base link")
+	}
+
+	var sitemapLinks []string
+	var pathPrefix string
+	pathParts := strings.Split(parsedBaseLink.Path, "/")
+	for _, pathPart := range pathParts[:len(pathParts)-1] {
+		if pathPart != "" {
+			pathPrefix += "/" + pathPart
+		}
+
+		parsedSitemapLink := &url.URL{
+			Scheme: parsedBaseLink.Scheme,
+			User:   parsedBaseLink.User,
+			Host:   parsedBaseLink.Host,
+			Path:   pathPrefix + "/sitemap.xml",
+		}
+		sitemapLinks = append(sitemapLinks, parsedSitemapLink.String())
+	}
+
+	return sitemapLinks, nil
 }
