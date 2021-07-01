a117a17d1a91e2ba0045aa945843eb49cdbe928d
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-07-01 23:00:00 +0300

    Add the urlutils.CompareLinkHosts() function

--- url-utils/compare_link_hosts.go
+++ url-utils/compare_link_hosts.go
@@ -1,5 +1,11 @@
 package urlutils
 
+import (
+	"net/url"
+
+	"github.com/pkg/errors"
+)
+
 // ComparisonResult ...
 type ComparisonResult int
 
@@ -8,3 +14,28 @@ const (
 	Same ComparisonResult = iota
 	Different
 )
+
+// CompareLinkHosts ...
+func CompareLinkHosts(linkOne string, linkTwo string) (
+	ComparisonResult,
+	error,
+) {
+	parsedLinkOne, err := url.Parse(linkOne)
+	if err != nil {
+		return 0, errors.Wrapf(err, "unable to parse link %q", linkOne)
+	}
+
+	parsedLinkTwo, err := url.Parse(linkTwo)
+	if err != nil {
+		return 0, errors.Wrapf(err, "unable to parse link %q", linkTwo)
+	}
+
+	var result ComparisonResult
+	if parsedLinkOne.Host == parsedLinkTwo.Host {
+		result = Same
+	} else {
+		result = Different
+	}
+
+	return result, nil
+}
