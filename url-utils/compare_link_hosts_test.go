2347feb4c39b6d68065070b1e1d43945ed3f4315
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-07-01 23:00:00 +0300

    Implement the tests of the urlutils.CompareLinkHosts() function

--- url-utils/compare_link_hosts_test.go
+++ url-utils/compare_link_hosts_test.go
@@ -18,7 +18,42 @@ func TestCompareLinkHosts(test *testing.T) {
 		wantResult ComparisonResult
 		wantErr    assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success with same links",
+			args: args{
+				linkOne: "http://example.com/",
+				linkTwo: "http://example.com/test",
+			},
+			wantResult: Same,
+			wantErr:    assert.NoError,
+		},
+		{
+			name: "success with different links",
+			args: args{
+				linkOne: "http://example1.com/",
+				linkTwo: "http://example2.com/test",
+			},
+			wantResult: Different,
+			wantErr:    assert.NoError,
+		},
+		{
+			name: "error with link one",
+			args: args{
+				linkOne: ":",
+				linkTwo: "http://example.com/test",
+			},
+			wantResult: 0,
+			wantErr:    assert.Error,
+		},
+		{
+			name: "error with link two",
+			args: args{
+				linkOne: "http://example.com/",
+				linkTwo: ":",
+			},
+			wantResult: 0,
+			wantErr:    assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			gotResult, gotErr := CompareLinkHosts(data.args.linkOne, data.args.linkTwo)
