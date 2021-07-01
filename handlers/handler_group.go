84cd20e032af48fa193c996abccf5009a37765b0
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-07-01 23:00:00 +0300

    Add the handlers.HandlerGroup.HandleLink() method

--- handlers/handler_group.go
+++ handlers/handler_group.go
@@ -1,8 +1,30 @@
 package handlers
 
 import (
+	"context"
+	"sync"
+
 	"github.com/thewizardplusplus/go-crawler/models"
 )
 
 // HandlerGroup ...
 type HandlerGroup []models.LinkHandler
+
+// HandleLink ...
+func (handlers HandlerGroup) HandleLink(
+	ctx context.Context,
+	link models.SourcedLink,
+) {
+	var waiter sync.WaitGroup
+	waiter.Add(len(handlers))
+
+	for _, handler := range handlers {
+		go func(handler models.LinkHandler) {
+			defer waiter.Done()
+
+			handler.HandleLink(ctx, link)
+		}(handler)
+	}
+
+	waiter.Wait()
+}
