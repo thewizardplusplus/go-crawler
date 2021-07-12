2298289f0c570ee4c7817f97f5afc154afac6cd1
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-07-12 23:00:00 +0300

    Add the extractors.TrimmingExtractor.ExtractLinks() method

--- extractors/trimming_extractor.go
+++ extractors/trimming_extractor.go
@@ -1,6 +1,9 @@
 package extractors
 
 import (
+	"context"
+
+	"github.com/pkg/errors"
 	"github.com/thewizardplusplus/go-crawler/models"
 	urlutils "github.com/thewizardplusplus/go-crawler/url-utils"
 )
@@ -9,4 +12,24 @@ import (
 type TrimmingExtractor struct {
 	TrimLink      urlutils.LinkTrimming
 	LinkExtractor models.LinkExtractor
+}
+
+// ExtractLinks ...
+func (extractor TrimmingExtractor) ExtractLinks(
+	ctx context.Context,
+	threadID int,
+	link string,
+) ([]string, error) {
+	links, err := extractor.LinkExtractor.ExtractLinks(ctx, threadID, link)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to extract links")
+	}
+
+	var trimmedLinks []string
+	for _, link := range links {
+		trimmedLink := urlutils.ApplyLinkTrimming(link, extractor.TrimLink)
+		trimmedLinks = append(trimmedLinks, trimmedLink)
+	}
+
+	return trimmedLinks, nil
 }
