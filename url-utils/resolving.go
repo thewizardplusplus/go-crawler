ba3c279593abd97b69ad66327acb327e9eceda56
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-08-26 23:00:00 +0300

    Add the urlutils.LinkResolver.ResolveLink() method

--- url-utils/resolving.go
+++ url-utils/resolving.go
@@ -2,9 +2,22 @@ package urlutils
 
 import (
 	"net/url"
+
+	"github.com/pkg/errors"
 )
 
 // LinkResolver ...
 type LinkResolver struct {
 	BaseLink *url.URL
+}
+
+// ResolveLink ...
+func (resolver LinkResolver) ResolveLink(link string) (string, error) {
+	parsedLink, err := url.Parse(link)
+	if err != nil {
+		return "", errors.Wrap(err, "unable to parse the link")
+	}
+
+	resolvedLink := resolver.BaseLink.ResolveReference(parsedLink)
+	return resolvedLink.String(), nil
 }
