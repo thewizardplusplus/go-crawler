01af1a7a2e021dc268e02b2e8eae6ca0e838f24b
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-08-26 23:00:00 +0300

    Implement the tests of the urlutils.LinkResolver.ResolveLink() method

--- url-utils/resolving_test.go
+++ url-utils/resolving_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLinkResolver_ResolveLink(test *testing.T) {
@@ -22,7 +23,70 @@ func TestLinkResolver_ResolveLink(test *testing.T) {
 		wantLink string
 		wantErr  assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success with a relative link (relative to the root)",
+			fields: fields{
+				BaseLink: func() *url.URL {
+					baseLink, err := url.Parse("http://example.com/one/")
+					require.NoError(test, err)
+
+					return baseLink
+				}(),
+			},
+			args: args{
+				link: "/two",
+			},
+			wantLink: "http://example.com/two",
+			wantErr:  assert.NoError,
+		},
+		{
+			name: "success with a relative link (relative to the current directory)",
+			fields: fields{
+				BaseLink: func() *url.URL {
+					baseLink, err := url.Parse("http://example.com/one/")
+					require.NoError(test, err)
+
+					return baseLink
+				}(),
+			},
+			args: args{
+				link: "two",
+			},
+			wantLink: "http://example.com/one/two",
+			wantErr:  assert.NoError,
+		},
+		{
+			name: "success with an absolute link",
+			fields: fields{
+				BaseLink: func() *url.URL {
+					baseLink, err := url.Parse("http://example-1.com/one/")
+					require.NoError(test, err)
+
+					return baseLink
+				}(),
+			},
+			args: args{
+				link: "http://example-2.com/two",
+			},
+			wantLink: "http://example-2.com/two",
+			wantErr:  assert.NoError,
+		},
+		{
+			name: "error",
+			fields: fields{
+				BaseLink: func() *url.URL {
+					baseLink, err := url.Parse("http://example.com/one/")
+					require.NoError(test, err)
+
+					return baseLink
+				}(),
+			},
+			args: args{
+				link: ":",
+			},
+			wantLink: "",
+			wantErr:  assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			resolver := LinkResolver{
