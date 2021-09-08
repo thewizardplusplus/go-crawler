753d7c2dfc4ff10e1a634ba92b3de20852288d01
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-08 23:00:00 +0300

    Add the extractors.BaseTagFilters constant

---
+++ extractors/base_tag_builder.go
@@ -0,0 +1,10 @@
+package extractors
+
+import (
+	htmlselector "github.com/thewizardplusplus/go-html-selector"
+)
+
+// BaseTagFilters ...
+var BaseTagFilters = htmlselector.OptimizeFilters(htmlselector.FilterGroup{
+	"base": {"href"},
+})
