c72d66fc5270c3b3fa9f0a2a9a126f34875d9ba3
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-08 23:00:00 +0300

    Add the urlutils.NewLinkResolver() function

--- url-utils/resolving.go
+++ url-utils/resolving.go
@@ -11,6 +11,36 @@ type LinkResolver struct {
 	BaseLink *url.URL
 }
 
+// NewLinkResolver ...
+func NewLinkResolver(baseLinks []string) (LinkResolver, error) {
+	var parsedBaseLinks []*url.URL
+	for _, baseLink := range baseLinks {
+		parsedBaseLink, err := url.Parse(baseLink)
+		if err != nil {
+			return LinkResolver{},
+				errors.Wrapf(err, "unable to parse base link %q", baseLink)
+		}
+
+		parsedBaseLinks = append(parsedBaseLinks, parsedBaseLink)
+		if parsedBaseLink.IsAbs() {
+			break
+		}
+	}
+
+	var totalBaseLink *url.URL
+	for index := len(parsedBaseLinks) - 1; index >= 0; index-- {
+		parsedBaseLink := parsedBaseLinks[index]
+		if totalBaseLink != nil {
+			totalBaseLink = totalBaseLink.ResolveReference(parsedBaseLink)
+		} else {
+			totalBaseLink = parsedBaseLink
+		}
+	}
+
+	resolver := LinkResolver{BaseLink: totalBaseLink}
+	return resolver, nil
+}
+
 // ResolveLink ...
 func (resolver LinkResolver) ResolveLink(link string) (string, error) {
 	parsedLink, err := url.Parse(link)
