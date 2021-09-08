9fa1495839f90921f1fe5b561dffaea6a4e78203
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-08 23:00:00 +0300

    Add the extractors.BaseTagBuilder structure

--- extractors/base_tag_builder.go
+++ extractors/base_tag_builder.go
@@ -8,3 +8,9 @@ import (
 var BaseTagFilters = htmlselector.OptimizeFilters(htmlselector.FilterGroup{
 	"base": {"href"},
 })
+
+// BaseTagBuilder ...
+type BaseTagBuilder struct {
+	baseLink     []byte
+	isFirstFound bool
+}
