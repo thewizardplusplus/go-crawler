6f5a7fae0e75a82f73114ed23154456d03d06240
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-28 23:00:00 +0300

    Add the transformers.TrimmingTransformer.TransformLinks() method

--- extractors/transformers/trimming_transformer.go
+++ extractors/transformers/trimming_transformer.go
@@ -1,10 +1,27 @@
 package transformers
 
 import (
+	"net/http"
+
 	urlutils "github.com/thewizardplusplus/go-crawler/url-utils"
 )
 
 // TrimmingTransformer ...
 type TrimmingTransformer struct {
 	TrimLink urlutils.LinkTrimming
+}
+
+// TransformLinks ...
+func (transformer TrimmingTransformer) TransformLinks(
+	links []string,
+	response *http.Response,
+	responseContent []byte,
+) ([]string, error) {
+	var trimmedLinks []string
+	for _, link := range links {
+		trimmedLink := urlutils.ApplyLinkTrimming(link, transformer.TrimLink)
+		trimmedLinks = append(trimmedLinks, trimmedLink)
+	}
+
+	return trimmedLinks, nil
 }
