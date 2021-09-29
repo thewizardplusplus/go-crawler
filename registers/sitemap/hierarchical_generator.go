a861357518504e22748115c42200e366ae0badcd
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-29 23:00:00 +0300

    Improve the error handling in the sitemap.HierarchicalGenerator.ExtractLinks() method

--- registers/sitemap/hierarchical_generator.go
+++ registers/sitemap/hierarchical_generator.go
@@ -3,6 +3,7 @@ package sitemap
 import (
 	"context"
 
+	"github.com/pkg/errors"
 	urlutils "github.com/thewizardplusplus/go-crawler/url-utils"
 )
 
@@ -21,10 +22,15 @@ func (generator HierarchicalGenerator) ExtractLinks(
 	[]string,
 	error,
 ) {
-	return urlutils.GenerateHierarchicalLinks(
+	hierarchicalLinks, err := urlutils.GenerateHierarchicalLinks(
 		baseLink,
 		"sitemap.xml",
 		urlutils.SanitizeBaseLink(generator.SanitizeLink),
 		urlutils.WithMaximalHierarchyDepth(generator.MaximalDepth),
 	)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to generate the hierarchical links")
+	}
+
+	return hierarchicalLinks, nil
 }
