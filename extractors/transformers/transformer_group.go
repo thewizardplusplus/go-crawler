ce05b1bcee3566d4988ae9606747f62417c84ae8
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-29 23:00:00 +0300

    Add the transformers.TransformerGroup.TransformLinks() method

--- extractors/transformers/transformer_group.go
+++ extractors/transformers/transformer_group.go
@@ -1,8 +1,33 @@
 package transformers
 
 import (
+	"net/http"
+
+	"github.com/pkg/errors"
 	"github.com/thewizardplusplus/go-crawler/extractors"
 )
 
 // TransformerGroup ...
 type TransformerGroup []extractors.LinkTransformer
+
+// TransformLinks ...
+func (transformers TransformerGroup) TransformLinks(
+	links []string,
+	response *http.Response,
+	responseContent []byte,
+) ([]string, error) {
+	// processing of the transformers should be sequential
+	// to one transformer can influence to another one
+	transformedLinks := links
+	for index, transformer := range transformers {
+		var err error
+		transformedLinks, err =
+			transformer.TransformLinks(transformedLinks, response, responseContent)
+		if err != nil {
+			return nil,
+				errors.Wrapf(err, "unable to transform links via transformer #%d", index)
+		}
+	}
+
+	return transformedLinks, nil
+}
