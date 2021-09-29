dcc55ce5701ba281852664cbb0f567ca18c4ca83
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-29 23:00:00 +0300

    Implement the tests of the transformers.TransformerGroup.TransformLinks() method

--- extractors/transformers/transformer_group_test.go
+++ extractors/transformers/transformer_group_test.go
@@ -1,11 +1,16 @@
 package transformers
 
 import (
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
+	"testing/iotest"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/thewizardplusplus/go-crawler/extractors"
 )
 
 func TestTransformerGroup_TransformLinks(test *testing.T) {
@@ -22,7 +27,162 @@ func TestTransformerGroup_TransformLinks(test *testing.T) {
 		wantLinks    []string
 		wantErr      assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name:         "success without transformers",
+			transformers: nil,
+			args: args{
+				links: []string{"http://example.com/1", "http://example.com/2"},
+				response: &http.Response{
+					Body: ioutil.NopCloser(strings.NewReader(`
+						<ul>
+							<li><a href="http://example.com/1">1</a></li>
+							<li><a href="http://example.com/2">2</a></li>
+						</ul>
+					`)),
+					Request: httptest.NewRequest(http.MethodGet, "http://example.com/", nil),
+				},
+				responseContent: []byte(`
+					<ul>
+						<li><a href="http://example.com/1">1</a></li>
+						<li><a href="http://example.com/2">2</a></li>
+					</ul>
+				`),
+			},
+			wantLinks: []string{"http://example.com/1", "http://example.com/2"},
+			wantErr:   assert.NoError,
+		},
+		{
+			name: "success with transformers",
+			transformers: TransformerGroup{
+				func() extractors.LinkTransformer {
+					links := []string{"http://example.com/1", "http://example.com/2"}
+					transformedLinks := []string{
+						"http://example.com/1/transformed/1",
+						"http://example.com/2/transformed/1",
+					}
+
+					responseContent := `
+						<ul>
+							<li><a href="http://example.com/1">1</a></li>
+							<li><a href="http://example.com/2">2</a></li>
+						</ul>
+					`
+					response := &http.Response{
+						Body:    ioutil.NopCloser(strings.NewReader(responseContent)),
+						Request: httptest.NewRequest(http.MethodGet, "http://example.com/", nil),
+					}
+
+					transformer := new(MockLinkTransformer)
+					transformer.
+						On("TransformLinks", links, response, []byte(responseContent)).
+						Return(transformedLinks, nil)
+
+					return transformer
+				}(),
+				func() extractors.LinkTransformer {
+					links := []string{
+						"http://example.com/1/transformed/1",
+						"http://example.com/2/transformed/1",
+					}
+					transformedLinks := []string{
+						"http://example.com/1/transformed/2",
+						"http://example.com/2/transformed/2",
+					}
+
+					responseContent := `
+						<ul>
+							<li><a href="http://example.com/1">1</a></li>
+							<li><a href="http://example.com/2">2</a></li>
+						</ul>
+					`
+					response := &http.Response{
+						Body:    ioutil.NopCloser(strings.NewReader(responseContent)),
+						Request: httptest.NewRequest(http.MethodGet, "http://example.com/", nil),
+					}
+
+					transformer := new(MockLinkTransformer)
+					transformer.
+						On("TransformLinks", links, response, []byte(responseContent)).
+						Return(transformedLinks, nil)
+
+					return transformer
+				}(),
+			},
+			args: args{
+				links: []string{"http://example.com/1", "http://example.com/2"},
+				response: &http.Response{
+					Body: ioutil.NopCloser(strings.NewReader(`
+						<ul>
+							<li><a href="http://example.com/1">1</a></li>
+							<li><a href="http://example.com/2">2</a></li>
+						</ul>
+					`)),
+					Request: httptest.NewRequest(http.MethodGet, "http://example.com/", nil),
+				},
+				responseContent: func() []byte {
+					return []byte(`
+						<ul>
+							<li><a href="http://example.com/1">1</a></li>
+							<li><a href="http://example.com/2">2</a></li>
+						</ul>
+					`)
+				}(),
+			},
+			wantLinks: []string{
+				"http://example.com/1/transformed/2",
+				"http://example.com/2/transformed/2",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "error",
+			transformers: TransformerGroup{
+				func() extractors.LinkTransformer {
+					links := []string{"http://example.com/1", "http://example.com/2"}
+
+					responseContent := `
+						<ul>
+							<li><a href="http://example.com/1">1</a></li>
+							<li><a href="http://example.com/2">2</a></li>
+						</ul>
+					`
+					response := &http.Response{
+						Body:    ioutil.NopCloser(strings.NewReader(responseContent)),
+						Request: httptest.NewRequest(http.MethodGet, "http://example.com/", nil),
+					}
+
+					transformer := new(MockLinkTransformer)
+					transformer.
+						On("TransformLinks", links, response, []byte(responseContent)).
+						Return(nil, iotest.ErrTimeout)
+
+					return transformer
+				}(),
+				new(MockLinkTransformer),
+			},
+			args: args{
+				links: []string{"http://example.com/1", "http://example.com/2"},
+				response: &http.Response{
+					Body: ioutil.NopCloser(strings.NewReader(`
+						<ul>
+							<li><a href="http://example.com/1">1</a></li>
+							<li><a href="http://example.com/2">2</a></li>
+						</ul>
+					`)),
+					Request: httptest.NewRequest(http.MethodGet, "http://example.com/", nil),
+				},
+				responseContent: func() []byte {
+					return []byte(`
+						<ul>
+							<li><a href="http://example.com/1">1</a></li>
+							<li><a href="http://example.com/2">2</a></li>
+						</ul>
+					`)
+				}(),
+			},
+			wantLinks: nil,
+			wantErr:   assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			gotLinks, gotErr := data.transformers.TransformLinks(
